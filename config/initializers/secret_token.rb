314b4d61108f58acdd8a56a1ea89b4408f04fb99
Marcelo Soares Souza <marcelo@juntadados.org>
2012-01-28 09:21:43 -0200

    * Initializing jCMS repository - closes #7

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jcms::Application.config.secret_token = '98c912ed034c45bf771dcdbef6cc790ccc4d779aaa42c8311893dd892177ce78755d0eda627a39f15136a21299888ec7dac0b2ba4d23b1e6cfe40515c6aee9ed'
