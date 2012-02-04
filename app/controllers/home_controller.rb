419ad95a7b5f5d5a15e1ccc628add5eaf591a1b7
Marcelo Soares Souza <marcelo@juntadados.org>
2012-02-04 11:59:59 -0200

    * layout getting good

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,7 @@ class HomeController < ApplicationController
   protect_from_forgery
 
   def index
-    @texts  = Text.all(:joins  => :contents, :conditions => ["contents.publish = true AND contents.firstpage = true"])
-    @images = Image.all(:joins => :contents, :conditions => ["contents.publish = true AND contents.firstpage = true"])
+    @texts  = Text.all(:joins  => :contents, :order => 'created_at DESC', :conditions => ["contents.publish = true AND contents.firstpage = true"])
 
     respond_to do |format|
       format.html # index.html.erb
